cc4e9cc3757c8a41549a1b141a2a975e64690a2a
mattr21 <matt.rothstein21@gmail.com>
2019-03-26 15:45:12 -0700

    configured knex, addedget, get by id, and post requests

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
 
-const server = express(); 
+const knexConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: './data/lambda.sqlite3',
+  }
+};
+
+const db = knex(knexConfig);
+
+const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.post('/api/zoos', async (req, res) => {
+  try {
+    const ids = await db('zoos')
+    .insert(req.body)
+    const id = ids[0]
+
+    const zoo = await db('zoos')
+      .where({ id })
+      .first();
+
+    res.status(201).json(zoo)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+});
+
+server.get('/api/zoos', async (req, res) => {
+  try {
+    const zoos = await db('zoos');
+    res.status(200).json(zoos);
+  } catch (error) {
+      res.status(500).json(error);
+  }
+})
+
+server.get('/api/zoos/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
+    .first()
+    .then(role => {
+      res.status(200).json(role);
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    });
+});
 
 const port = 3300;
 server.listen(port, function() {
